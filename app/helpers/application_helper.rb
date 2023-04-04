22066af5dbf775d2571982436c3b8df931336022
Hans Gamarra <hans.gamarra1303@gmail.com>
2023-04-04 15:17:46 -0500

    Preparing layout + enabling Bootstrap JS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+      when :notice then "alert alert-dismissible fade show alert-info"
+      when :success then "alert alert-dismissible fade show alert-success"
+      when :error then "alert alert-dismissible fade show alert-danger"
+      when :alert then "alert alert-dismissible fade show alert-danger"
+    end
+  end
+  
+  def active_text(link_controller, link_action = nil)
+    same_controller = params[:controller] == link_controller.to_s
+
+    if link_action.present?
+      same_action = params[:action] == link_action.to_s
+      same_controller && same_action ? 'text-danger' : ''
+    else
+      same_controller ? 'text-danger' : ''
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Running seeds...'
+ActiveRecord::Base.transaction do
+  # Superadmin user
+  user = User.find_or_initialize_by(email: 'admin@test.com')
+  if !user.persisted?
+    user.password = '123456'
+    user.password_confirmation = '123456'
+    user.save!
+  end
+rescue StandardError => e
+  puts "There was an error: #{e.message}"
+end
+puts 'Done!'
